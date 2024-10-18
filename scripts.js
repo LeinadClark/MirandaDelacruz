6cbbf51dbe6aa4ef5284eba00554477f883f8eca
Keylark <keylarksite@gmail.com>
2024-10-18 16:02:10 +0800

    Try

--- scripts.js
+++ scripts.js
@@ -2,6 +2,11 @@
 document.addEventListener("DOMContentLoaded", function() {
     var header = document.querySelector("header");
     header.classList.add("hidden");
+
+    // Reset the URL hash to prevent the modal from opening
+    if (window.location.hash === "#loginModal") {
+        window.location.hash = ''; // Reset the URL hash on page load
+    }
 });
 
 let lastScrollY = window.scrollY;
@@ -10,13 +15,10 @@ window.addEventListener("scroll", function () {
     var header = document.querySelector("header");
 
     if (window.scrollY === 0) {
-        
         header.classList.add("hidden");
     } else if (window.scrollY > lastScrollY) {
-    
         header.classList.add("hidden");
     } else {
-     
         header.classList.remove("hidden");
     }
     lastScrollY = window.scrollY;
@@ -24,9 +26,8 @@ window.addEventListener("scroll", function () {
 
 // Hero Page Controls
 const slider = document.querySelector('.slider');
-
-const leftArrow = document.querySelector('.left')
-const rightArrow = document.querySelector('.right')
+const leftArrow = document.querySelector('.left');
+const rightArrow = document.querySelector('.right');
 
 rightArrow.addEventListener('click', function() {
     slider.style.transform = 'translate(-25%)';
